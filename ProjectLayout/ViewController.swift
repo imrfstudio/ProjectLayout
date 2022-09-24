ca88ce6716fd2832aa52fdf20a4e8a9a4a2692ae
MJ <107776401+shift8studio@users.noreply.github.com>
2022-09-24 10:47:07 -0500

    Initial Commit

---
+++ ProjectLayout/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ProjectLayout
+//
+//  Created by MJ Michand on 9/24/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
